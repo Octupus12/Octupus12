0f78c7981a258135f2c83c8d208778277abd708d
Octupus12 <110430962+Octupus12@users.noreply.github.com>
2022-09-22 10:45:12 +0545

    Create re.m

---
+++ re.m
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-3">
+  <h2>Black Table</h2>
+  <p></p>            
+  <table class="table table-dark">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+        <th>Roll No.</th>
+        <th>Phone Number</th>
+        <th>Class</th>
+        <th>Date-of-birth</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Utsav</td>
+        <td>Rajbhandari</td>
+        <td>utsav@gamil.com</td>
+        <td>1</td>
+        <td>9845915850</td>
+        <td>12</td>
+        <td>July-05-2003</td>
+      </tr>
+      <tr>
+        <td>Maria</td>
+        <td>Joe</td>
+        <td>maria@gmail.com</td>
+        <td>2</td>
+        <td>984453210</td>
+        <td>11</td>
+        <td>june-04-2004</td>
+      </tr>
+      <tr>
+        <td>jack</td>
+        <td>DoN</td>
+        <td>jack@gamil.com</td>
+        <td>3</td>
+        <td>9845432451</td>
+        <td>12</td>
+        <td>March-21-2002</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
